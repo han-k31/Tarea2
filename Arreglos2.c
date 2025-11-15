76e1a00e0d60ca916a2e9119f60f83723b626349
han-k31 <hannasgiron@gmail.com>
2025-11-15 15:23:19 -0600

    Create Arreglos2.c

---
+++ Arreglos2.c
@@ -0,0 +1,12 @@
+/* Girón Knepp Hanna Sofía
+Tarea 2 arreglo 2
+construya un programa tal que dado como entrada un arreglo unidimen-
+sional de enteros y un n ́umero entero, determine cu ́antas veces se encuentra
+el n ́umero dentro del arreglo.*/
+
+#include <stdio.h>
+int main()
+{
+  
+  return 0;
+}

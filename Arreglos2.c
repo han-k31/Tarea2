2dfb39ae11c0ef633c92fdf08de048824c60d362
han-k31 <hannasgiron@gmail.com>
2025-11-21 13:51:23 -0600

    Update Arreglos2.c

--- Arreglos2.c
+++ Arreglos2.c
@@ -5,8 +5,40 @@ sional de enteros y un n ́umero entero, determine cu ́antas veces se encuentra
 el n ́umero dentro del arreglo.*/
 
 #include <stdio.h>
-int main()
+
+int cont(int arreglo[], int largo, int numero) 
 {
-  
-  return 0;
+    int contador = 0;
+    
+    for(int i = 0; i < largo; i++) 
+    {
+        if(arreglo[i] == numero) 
+        {
+            contador++;
+        }
+    }
+    
+    return contador;
+}
+
+int main() 
+{
+    int arreglo[] = {1, 6, 9, 2, 5, 5, 2, 8, 7, 2};
+    int tamaño = 10;
+    int numero;
+    
+    printf("Arreglo: ");
+    for(int i = 0; i < tamaño; i++) 
+    {
+        printf("%d ", arreglo[i]);
+    }
+
+    printf("\nIngrese el número a buscar: ");
+    scanf("%d", &numero);
+    
+    int lim = cont(arreglo, tamaño, numero);
+    
+    printf("El número %d aparece %d veces en el arreglo", numero, lim);
+    
+    return 0;
 }
